1981f62e00a145e13e52a1bbe66edb833f92ac30
kiratveersinghralhan-hue <kiratveersinghralhan@gmail.com>
2026-04-01 17:29:52 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -947,7 +947,7 @@ function initIntroLogo(){
     setTimeout(() => {
       const intro = document.getElementById("introLogo");
       if(intro) intro.style.display = "none";
-    }, 3400);
+    }, 2850);
   });
 }
 
